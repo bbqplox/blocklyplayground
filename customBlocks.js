ac97cb75a3eb66fac566b675db7e3d7ae8468617
Joseph Widjaja <jewidjaj@ucsd.edu>
2017-12-10 18:52:26 -0800

    animals working, messages show in console

--- customBlocks.js
+++ customBlocks.js
@@ -31,8 +31,34 @@ Blockly.Blocks['clearscreen'] = {
 Blockly.Blocks['catpose'] = {
     init: function() {
         this.appendDummyInput()
-            .appendField("Change Pose")
+            .appendField("Change Cat Pose")
             .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"]]), "catIndex");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(175);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['pigpose'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Change Pig Pose")
+            .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"]]), "pigIndex");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(175);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['saymessage'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Say")
+            .appendField(new Blockly.FieldTextInput('type here'), 'theText');
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
         this.setColour(175);
@@ -54,6 +80,20 @@ Blockly.JavaScript['clearscreen'] = function(block) {
 
 Blockly.JavaScript['catpose'] = function(block) {
     var dropdown_catindex = block.getFieldValue('catIndex');
-    var blockCode = 'updatePose('+dropdown_catindex+');';
+    var blockCode = 'updateCatPose('+dropdown_catindex+');';
+    return blockCode;
+};
+
+Blockly.JavaScript['pigpose'] = function(block) {
+    var dropdown_pigindex = block.getFieldValue('pigIndex');
+    var blockCode = 'updatePigPose('+dropdown_pigindex+');';
+    return blockCode;
+};
+
+Blockly.JavaScript['saymessage'] = function(block) {
+    var text = block.getFieldValue('theText');
+    console.log(text);
+    text = '\'' + text + '\''
+    var blockCode = 'updateMessage('+text+');';
     return blockCode;
 };
